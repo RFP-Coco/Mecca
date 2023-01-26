e0ca4188a811c910660488f57e6af0b4533d97e6
qchen <quanjingchen@gmail.com>
2023-01-26 12:30:30 -0800

    start working on index.js

--- server/index.js
+++ server/index.js
@@ -6,6 +6,11 @@ const PORT = 3000;
 
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
+app.get('/posts/:postID', (req, res) => {
+  
+
+});
+
 app.listen(PORT, () => {
   console.log(`Server Listening at http://localhost:${PORT}`);
 })
