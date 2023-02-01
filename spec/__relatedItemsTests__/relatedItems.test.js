af6b26769689ef9e61cf4966f004442fa3d5f4dd
Tom Spitz <tjspitz1986@gmail.com>
2023-01-31 21:00:09 -0600

    Wrote extremely basic tests. Mostly because I was frustrated with being unable to write an applicable test.

--- spec/__relatedItemsTests__/relatedItems.test.js
+++ spec/__relatedItemsTests__/relatedItems.test.js
@@ -1,48 +1,20 @@
 import React from 'react';
-
-import {
-  cleanup, test, render, screen, fireEevent,
-} from '@testing-library/react';
-
-import {
-  afterEach, describe, it, expect,
-} from '@jest/globals';
-
+import { cleanup, render, screen, within } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { afterEach, describe, it, expect } from '@jest/globals';
 import jest from 'jest';
 
-import RelProdContainer from '../../client/src/components/Sub_RelatedItems/RelProdContainer.jsx';
+import RelProdContainer from '../../client/src/components/Sub_RelatedItems/RelProdContainer';
 
-afterEach(cleanup);
-
-describe('Related Items', () => {
-  it('renders all related product cards to the DOM', () => {
-    const productID = 40348;
-    const { container } = (<RelProdContainer productID={productID} />);
-    console.log('container: ', container);
-    expect(container).toMatchSnapshot();
+describe('A very basic test born of exponential  frustration', () => {
+  it('understands truthy and falsy values', () => {
+    expect(0).toBeFalsy();
+    expect(undefined).toBeFalsy();
+    expect('a tapdancing hippopotamus').toBeTruthy();
+  });
+  it('understands length', () => {
+    expect('length').toHaveLength(6);
+    expect(['l', 'e', 'n', 'g', 't', 'h']).toHaveLength(6);
+    expect([0]).not.toHaveLength(0);
   });
 });
-
-// RelProdContainer.jsx
-
-// getRelatedProdsAndReviews should return a response data
-//   array length 7 when productID state is 40348
-
-// describe('Related Products Container', () => {
-//   test('response data is an array of length 7 when productID is 40348', () => {
-//     const id = 40348;
-//     return getRelatedProdsAndReviews()
-//       .then((response) => {
-//         expect(response.data).toHaveLength(7);
-//       });
-//     // const {container} = render(<RelProdContainer productID={40348})
-//   });
-// });
-
-// the relatedIDs state should not have duplicate IDs
-//   the relatedIDs state should be length 6 when productID state is 40348
-
-// the relatedProds state should be an array of same length as relatedIDs state
-
-// there should be the same # of 'SingleProd' components rendered as relatedProds' length
