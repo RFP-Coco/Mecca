dddfa7cb4e55fc084b80aadaf9d0f3aadda089ab
quanjingchen <quanjingchen@gmail.com>
2023-02-02 10:46:23 -0800

    refactor the style selector code with destructure

--- client/src/components/Sub_ProductDetail/StyleSelector.jsx
+++ client/src/components/Sub_ProductDetail/StyleSelector.jsx
@@ -3,6 +3,7 @@ import StyleList from './Sub_StyleSelector/StyleList.jsx';
 
 function StyleSelector({ productID, productStyle, currentStyle, setCurrentStyle }) {
   console.log('SHOW CURRENT STYLE: ', currentStyle);
+  const { sale_price, original_price, name } = currentStyle;
   const [startSelect, setStartSelect] = useState(false);
 
   useEffect(() => { setStartSelect(false); }, [productID]);
@@ -10,9 +11,14 @@ function StyleSelector({ productID, productStyle, currentStyle, setCurrentStyle 
   return (
     <div className="style-selector">
       <div className="price" style={{ padding: '1rem 0' }}>
-        {currentStyle.sale_price ? <p><span className="sale-price">${currentStyle.sale_price}</span> <span className="original-sale-price">${currentStyle.original_price}</span></p> : <p><span className="original-price">${currentStyle.original_price}</span></p>}
+        {sale_price
+          ? <p><span className="sale-price">${sale_price}</span> <span className="original-sale-price">${original_price}</span></p>
+          : <p><span className="original-price">${original_price}</span></p>}
       </div>
-      <div style={{ paddingBottom: '1rem' }}>STYLE {'>'} <span style={{ fontStyle: 'italic' }}>{startSelect ? currentStyle.name : 'SELECTED STYLE'}</span></div>
+      <div style={{ paddingBottom: '1rem' }}>
+        STYLE {'>'}
+        <span style={{ fontStyle: 'italic' }}>{startSelect ? name : 'SELECTED STYLE'}</span>
+      </div>
       <StyleList
         productStyle={productStyle}
         setCurrentStyle={setCurrentStyle}
