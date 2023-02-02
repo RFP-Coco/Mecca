2d37e51735d82ed1627b1a5d193c515902dd6047
Kevin Le <iamkevinvle@gmail.com>
2023-02-02 15:07:32 -0800

    implement product breakdown

---
+++ client/src/components/Sub_RatingsReviews/ProductDashboard.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+// import Slider from './Slider.jsx';
+
+export default function ProductDashboard({
+  reviewMetadata,
+}) {
+  const labels = {
+    size: ['a size too small', 'half size too small', 'perfect', 'half size too big', 'a size too wide'],
+    width: ['too narrow', 'slightly narrow', 'perfect', 'slightly wide', 'too wide'],
+    comfort: ['uncomfortable', 'slightly uncomfortable', 'just ok', 'comfortable', 'perfect'],
+    quality: ['very poor', 'below average', 'what i expected', 'pretty great', 'perfect'],
+    length: ['runs short', 'runs slightly short', 'perfect', 'runs slightly long', 'runs long'],
+    fit: ['runs tight', 'runs slightly tight', 'perfect', 'runs slightly long', 'runs long'],
+  };
+
+  const { characteristics } = reviewMetadata;
+
+  return (
+    <div className="review-product-dashboard">
+      {Object.entries(characteristics)
+        .map((characteristic) => (
+          <>
+            <div
+              key={characteristic[1].id}
+              className="characteristic"
+            >
+              {characteristic[0]}
+            </div>
+            <div className="characteristics-slider">
+              <input
+                type="range"
+                min="1"
+                max="5"
+                value={characteristic[1].value}
+                list="labels"
+                readOnly
+              />
+              <datalist id="labels">
+                {labels[characteristic[0].toLowerCase()]
+                  .map((label, index) => (
+                    <option
+                      className="characteristics-label"
+                      value={index + 1}
+                      label={label}
+                    />
+                  ))}
+              </datalist>
+            </div>
+          </>
+        ))}
+    </div>
+  );
+}
