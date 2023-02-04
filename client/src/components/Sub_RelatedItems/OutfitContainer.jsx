a90bf029aa0c2f6fa87f1b191a8bb94fe2950b1f
Tom Spitz <tjspitz1986@gmail.com>
2023-02-04 15:58:29 -0600

    Products maintain their pic and info.

--- client/src/components/Sub_RelatedItems/OutfitContainer.jsx
+++ client/src/components/Sub_RelatedItems/OutfitContainer.jsx
@@ -2,34 +2,36 @@ import React, { useState, useEffect } from 'react';
 import SingleProd from './SingleProd.jsx';
 
 export default function OutfitContainer({
-  parentProduct, parentProductID, setParentProductID,
-  parentProductStyle, currentParentProductStyle, parentReviewMetadata, setAsNewOverview, setAllowCardClick,
+  parentProduct, setParentProductID,
+  parentProductStyle, currentParentProductStyle,
+  parentReviewMetadata, setAsNewOverview, setAllowCardClick,
 }) {
 
   // const { id } = thisProduct;
 
-  const [myOutfits, setMyOutfits] = useState(new Set());
+  // const [myOutfits, setMyOutfits] = useState(new Set());
+
+  const [myOutfits, setMyOutfits] = useState([]);
+  const [checkStyles, setCheckStyles] = useState({});
+
   const [outfitsUpdated, setOutfitsUpdated] = useState(false);
 
   useEffect(() => {
-    // if (myOutfits.size) handleAddOutfit();
-    // renderOutfits();
-    if (myOutfits.size) {
-      setMyOutfits(myOutfits.add(parentProduct));
+    if (myOutfits.length) {
       setOutfitsUpdated(false);
     }
   }, [outfitsUpdated]);
 
   const handleAddOutfit = (e) => {
     e.preventDefault();
-    // const { style_id } = currentParentProductStyle;
-    // console.log(`i'ma add ${style_id} to your collection`);
-    // setMyOutfits(myOutfits.add(currentParentProductStyle));
+    const { style_id } = currentParentProductStyle;
+    const myOutfitEntry = Object.assign(parentProduct, { style_id });
 
-    const { name } = parentProduct;
-    console.log(`i'ma add ${name} to your collection`);
+    if (checkStyles[style_id]) return;
 
-    setMyOutfits(myOutfits.add(parentProduct));
+    setCheckStyles(Object.assign(checkStyles, checkStyles[style_id] = 1));
+
+    setMyOutfits(myOutfits.concat([myOutfitEntry]));
     setOutfitsUpdated(true);
   };
 
@@ -37,12 +39,11 @@ export default function OutfitContainer({
     <div className="scrollable container">
       <AddOutfitCard
         parentProduct={parentProduct}
-        // currentParentProductStyle={currentParentProductStyle}
         myOutfits={myOutfits}
         setMyOutfits={setMyOutfits}
         handleAddOutfit={handleAddOutfit}
       />
-      {[...myOutfits].map((thisProduct) => (
+      {myOutfits.map((thisProduct) => (
         <SingleProd
           key={thisProduct.id}
           parentProduct={parentProduct}
