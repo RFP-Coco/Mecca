a71855d3e38da32b9bb44d709460a4dce7be7534
quanjingchen <quanjingchen@gmail.com>
2023-02-09 13:23:36 -0800

    more css

--- client/src/components/Head.jsx
+++ client/src/components/Head.jsx
@@ -6,6 +6,7 @@ function Head() {
   return (
     <header className="header">
       <h1>MECCA</h1>
+      <div className="free-shipping">Free shipping on orders of $60 or more</div>
       <div className="search-container">
         <form action="" onSubmit={(event) => {event.preventDefault()}}>
           <input type="search" placeholder="Search Mecca" className="search-field" />
